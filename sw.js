ddb9c86275a07625034f32ff9cea250ff4256bb4
m94elizondo-afk <m94elizondo@gmail.com>
2026-01-03 13:44:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
 /* Gym Tracker PWA - Service Worker */
-const CACHE = "gym-tracker-v5";
-const ASSETS = ["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE = "gym-tracker-v6";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("activate",(e)=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+      )
+      .then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener("fetch",(e)=>{
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(resp=>{
-    const copy = resp.clone();
-    caches.open(CACHE).then(cache=>cache.put(e.request,copy)).catch(()=>{});
-    return resp;
-  }).catch(()=>cached)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached ||
+      fetch(e.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(e.request, copy)).catch(() => {});
+          return resp;
+        })
+        .catch(() => cached)
+    )
+  );
 });
